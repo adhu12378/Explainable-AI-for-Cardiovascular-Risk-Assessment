123da6f6440030554fa1ad218b0e82eff235f9f2
Advaith Sunil <advaithsunil@Advaiths-MacBook-Air.local>
2026-09-13 12:51:48 +0100

    Update default fallback port to 10000 for Render

--- app.py
+++ app.py
@@ -1954,5 +1954,5 @@ def download_report():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
