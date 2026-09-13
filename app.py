32315e381f17cfe6c19d975732f6e0c7c16dee22
Advaith Sunil <advaithsunil@Advaiths-MacBook-Air.local>
2026-09-13 11:52:40 +0100

    Fix double underscores for main function

--- app.py
+++ app.py
@@ -1952,7 +1952,7 @@ def download_report():
 # APPLICATION START
 # ============================================================
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
